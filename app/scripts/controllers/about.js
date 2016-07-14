4118ef297accdc521bcbb13c06e7853afdf8dfd8
jessy.zheng@pwc.com <jessy.zheng@pwc.com>
2016-07-14 07:57:07 -0400

    modified

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -16,11 +16,28 @@ angular.module('learnAngularApp')
     ];
     getAdj($scope)
   }
-);
+)
+// 自定义过滤器
+.filter('fromFrewToMuch', function() {
+  return function(input) {
+    return input.sort((a, b) => {
+      return a >= b ? 1 : -1
+    })
+  }
+})
 
 function getAdj($scope) {
   $scope.adj = {
-    presonality: 'strong'
+    presonality: 'strong',
   };
+  $scope.date = new Date(),
   console.log($scope);
+  $scope.num = [23, 21, 43, 69, 55, 28, 12];
+  $scope.findLuckNum = function(num) {
+    return num % 3 === 0
+  }
+  // $scope.fromFrewToMuch = function(num) {
+  //   // console.log(num);
+  //
+  // }
 }
--- app/scripts/controllers/eventBind.js
+++ app/scripts/controllers/eventBind.js
@@ -8,11 +8,33 @@
  * Controller of the learnAngularApp
  */
 angular.module('learnAngularApp')
-  .controller('EventBindCtrl', function ($scope) {
+  .controller('EventBindCtrl', function ($scope, $interpolate) {
+    console.log($interpolate);
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-    $scope.name = 'wang'
+    $scope.counter = 10;
+    $scope.addIt = function() {
+      if ($scope.counter >= 0 && $scope.counter <= 19) {
+        $scope.counter ++
+      } else {
+        alert('can\'exceed 20')
+      }
+    };
+    $scope.minusIt = function() {
+      if ($scope.counter > 0 && $scope.counter <= 20) {
+        $scope.counter --
+      } else {
+        alert('can\'under 0')
+      }
+    };
+    $scope.$watch('emailBody', function(body) {
+      if (body) {
+        let template = $interpolate(body)
+        console.log(template);
+        $scope.previewText = template({to: $scope.to})
+      }
+    })
   });
